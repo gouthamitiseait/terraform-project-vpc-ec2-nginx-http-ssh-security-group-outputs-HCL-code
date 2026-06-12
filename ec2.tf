18404c1f5230084ee8ef9ced086bf71d6ab569ba
Gouthami T <151714607+gouthamitiseait@users.noreply.github.com>
2026-06-12 18:10:25 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,25 +1,23 @@
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-
-  owners = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["al2023-ami-*"]
-  }
-}
 
 resource "aws_instance" "nginx-server" {
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t3.micro"
-  subnet_id              = aws_subnet.public-subnet.id
-  vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+    ami="ami-0c4fc5dcabc9df21d"
+    instance_type="t3.micro"
+    subnet_id=aws_subnet.public-subnet.id
+    vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+    associate_public_ip_address = true
+    user_data = <<-EOF
+              #!/bin/bash
+              sudo yum upgrade -y && sudo yum update -y
+              sudo yum install nginx -y
+              sudo systemctl start nginx
+              sudo systemctl enable nginx
+              sudo systemctl status nginx
+              EOF
 
-  user_data = <<-EOF
-#!/bin/bash
-yum update -y
-yum install nginx -y
-systemctl start nginx
-systemctl enable nginx
-EOF
+    
+
+    tags ={
+        Name="nginx-server"
+    }
+  
 }
